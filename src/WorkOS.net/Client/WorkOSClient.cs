63b49c38e312dadfce9f1e619527589fef1d7026
Rohan Jadvani <rohan@workos.com>
2021-04-07 17:52:34 -0500

    0.9.4

--- src/WorkOS.net/Client/WorkOSClient.cs
+++ src/WorkOS.net/Client/WorkOSClient.cs
@@ -32,7 +32,7 @@
         /// <summary>
         /// Describes the .NET SDK version.
         /// </summary>
-        public static string SdkVersion => "0.9.3";
+        public static string SdkVersion => "0.9.4";
 
         /// <summary>
         /// Default timeout for HTTP requests.
