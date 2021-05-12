325e5bc5e7791866a18eb7793e123eafeedffcc3
Marshall Bowers <marshall@workos.com>
2021-05-11 22:16:14 -0400

    Move Organization operations to the top-level (#42)

---
+++ src/WorkOS.net/Services/Organizations/OrganizationsService.cs
@@ -0,0 +1,95 @@
+namespace WorkOS
+{
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A service that offers methods to interact with WorkOS Organizations.
+    /// </summary>
+    public class OrganizationsService : Service
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrganizationsService"/> class.
+        /// </summary>
+        public OrganizationsService()
+            : base(null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrganizationsService"/> class.
+        /// </summary>
+        /// <param name="client">A client used to make requests to WorkOS.</param>
+        public OrganizationsService(WorkOSClient client)
+            : base(client)
+        {
+        }
+
+        /// <summary>
+        /// Fetches a list of Organizations.
+        /// </summary>
+        /// <param name="options">Filter options when searching for Organizations.</param>
+        /// <param name="cancellationToken">
+        /// An optional token to cancel the request.
+        /// </param>
+        /// <returns>A paginated list of Organizations.</returns>
+        public async Task<WorkOSList<Organization>> ListOrganizations(
+            ListOrganizationsOptions options = null,
+            CancellationToken cancellationToken = default)
+        {
+            var request = new WorkOSRequest
+            {
+                Options = options,
+                Method = HttpMethod.Get,
+                Path = "/organizations",
+            };
+
+            return await this.Client.MakeAPIRequest<WorkOSList<Organization>>(request, cancellationToken);
+        }
+
+        /// <summary>
+        /// Creates an Organization.
+        /// </summary>
+        /// <param name="options">Parameters to create an Organization.</param>
+        /// <param name="cancellationToken">
+        /// An optional token to cancel the request.
+        /// </param>
+        /// <returns>The created Organization.</returns>
+        public async Task<Organization> CreateOrganization(
+            CreateOrganizationOptions options,
+            CancellationToken cancellationToken = default)
+        {
+            var request = new WorkOSRequest
+            {
+                Options = options,
+                Method = HttpMethod.Post,
+                Path = "/organizations",
+            };
+
+            return await this.Client.MakeAPIRequest<Organization>(request, cancellationToken);
+        }
+
+        /// <summary>
+        /// Updates an Organization.
+        /// </summary>
+        /// <param name="options">Parameters to update an Organization.</param>
+        /// <param name="cancellationToken">
+        /// An optional token to cancel the request.
+        /// </param>
+        /// <returns>The updated Organization.</returns>
+        public async Task<Organization> UpdateOrganization(
+          UpdateOrganizationOptions options,
+          CancellationToken cancellationToken = default)
+        {
+            var request = new WorkOSRequest
+            {
+                Options = options,
+                Method = HttpMethod.Put,
+                Path = $"/organizations/{options.Organization}",
+            };
+
+            return await this.Client.MakeAPIRequest<Organization>(request, cancellationToken);
+        }
+    }
+}
---
+++ test/WorkOSTests/Services/Organizations/OrganizationsServiceTest.cs
@@ -0,0 +1,133 @@
+namespace WorkOSTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using Newtonsoft.Json;
+    using WorkOS;
+    using Xunit;
+
+    public class OrganizationsServiceTest
+    {
+        private readonly HttpMock httpMock;
+
+        private readonly OrganizationsService service;
+
+        private readonly CreateOrganizationOptions createOrganizationOptions;
+
+        private readonly ListOrganizationsOptions listOrganizationsOptions;
+
+        private readonly UpdateOrganizationOptions updateOrganizationOptions;
+
+        private readonly Organization mockOrganization;
+
+        public OrganizationsServiceTest()
+        {
+            this.httpMock = new HttpMock();
+            var client = new WorkOSClient(
+                new WorkOSOptions
+                {
+                    ApiKey = "test",
+                    HttpClient = this.httpMock.HttpClient,
+                });
+            this.service = new OrganizationsService(client);
+
+            this.createOrganizationOptions = new CreateOrganizationOptions
+            {
+                Name = "Foo Corp",
+                Domains = new string[]
+                {
+                    "foo-corp.com",
+                },
+            };
+
+            this.updateOrganizationOptions = new UpdateOrganizationOptions
+            {
+                Organization = "org_123",
+                Domains = new string[]
+                {
+                    "foo-corp.com",
+                },
+                Name = "Foo Corp 2",
+            };
+
+            this.listOrganizationsOptions = new ListOrganizationsOptions
+            {
+                Domains = new string[]
+                {
+                    "foo-corp.com",
+                },
+            };
+
+            this.mockOrganization = new Organization
+            {
+                Id = "org_123",
+                Name = "Foo Corp",
+                Domains = new OrganizationDomain[]
+                {
+                    new OrganizationDomain
+                    {
+                        Id = "org_domain_123",
+                        Domain = "foo-corp.com",
+                    },
+                },
+            };
+        }
+
+        [Fact]
+        public async void TestListOrganizations()
+        {
+            var mockResponse = new WorkOSList<Organization>
+            {
+                Data = new List<Organization>
+                {
+                    this.mockOrganization,
+                },
+            };
+            this.httpMock.MockResponse(
+                HttpMethod.Get,
+                "/organizations",
+                HttpStatusCode.OK,
+                RequestUtilities.ToJsonString(mockResponse));
+            var response = await this.service.ListOrganizations(this.listOrganizationsOptions);
+
+            this.httpMock.AssertRequestWasMade(HttpMethod.Get, "/organizations");
+            Assert.Equal(
+                JsonConvert.SerializeObject(mockResponse),
+                JsonConvert.SerializeObject(response));
+        }
+
+        [Fact]
+        public async void TestCreateOrganization()
+        {
+            this.httpMock.MockResponse(
+                HttpMethod.Post,
+                "/organizations",
+                HttpStatusCode.Created,
+                RequestUtilities.ToJsonString(this.mockOrganization));
+            var response = await this.service.CreateOrganization(this.createOrganizationOptions);
+
+            this.httpMock.AssertRequestWasMade(HttpMethod.Post, "/organizations");
+            Assert.Equal(
+                JsonConvert.SerializeObject(this.mockOrganization),
+                JsonConvert.SerializeObject(response));
+        }
+
+        [Fact]
+        public async void TestUpdateOrganization()
+        {
+            this.httpMock.MockResponse(
+                HttpMethod.Put,
+                $"/organizations/{this.updateOrganizationOptions.Organization}",
+                HttpStatusCode.OK,
+                RequestUtilities.ToJsonString(this.mockOrganization));
+            var response = await this.service.UpdateOrganization(this.updateOrganizationOptions);
+
+            this.httpMock.AssertRequestWasMade(HttpMethod.Put, $"/organizations/{this.updateOrganizationOptions.Organization}");
+            Assert.Equal(
+                JsonConvert.SerializeObject(this.mockOrganization),
+                JsonConvert.SerializeObject(response));
+        }
+    }
+}
