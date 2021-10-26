494357a517a61c39ea03e6eade09d8053faf3088
David <86431031+dliu-workos@users.noreply.github.com>
2021-10-25 20:13:32 -0700

    Update User.cs

--- src/WorkOS.net/Services/DirectorySync/Entities/User.cs
+++ src/WorkOS.net/Services/DirectorySync/Entities/User.cs
@@ -97,29 +97,5 @@
             [JsonProperty("type")]
             public string Type { get; set; }
         }
-
-        /// <summary>
-        /// Contains data about a User's groups.
-        /// </summary>
-        public class Group
-        {
-            /// <summary>
-            /// Description of the record.
-            /// </summary>
-            [JsonProperty("object")]
-            public const string Object = "directory_group";
-
-            /// <summary>
-            /// The Group's identifier.
-            /// </summary>
-            [JsonProperty("id")]
-            public string Id { get; set; }
-
-            /// <summary>
-            /// The Group's name.
-            /// </summary>
-            [JsonProperty("name")]
-            public string Name { get; set; }
-        }
     }
 }
