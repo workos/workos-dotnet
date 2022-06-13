1005cd44e3d62e5b40a6939ae542b9af0dcdab71
Patrick_Liao <patrick@workos.com>
2022-06-13 09:15:33 -0500

    Add new providers and bump version

--- src/WorkOS.net/Services/SSO/Enums/ConnectionType.cs
+++ src/WorkOS.net/Services/SSO/Enums/ConnectionType.cs
@@ -13,14 +13,30 @@
         [EnumMember(Value = "ADFSSAML")]
         ADFSSAML,
 
+        [EnumMember(Value = "AdpOidc")]
+        AdpOidc,
+
         [EnumMember(Value = "Auth0SAML")]
         Auth0SAML,
 
         [EnumMember(Value = "AzureSAML")]
         AzureSAML,
 
+        [EnumMember(Value = "CasSAML")]
+        CasSAML,
+
+        [EnumMember(Value = "CloudflareSAML")]
+        CloudflareSAML,
+
+
+        [EnumMember(Value = "ClasslinkSAML")]
+        ClassLinkSAML,
+
         [EnumMember(Value = "CyberArkSAML")]
         CyberArkSAML,
+
+        [EnumMember(Value = "DuoSAML")]
+        DuoSAML,
 
         [EnumMember(Value = "GenericOIDC")]
         GenericOIDC,
@@ -37,8 +53,23 @@
         [EnumMember(Value = "JumpCloudSAML")]
         JumpCloudSAML,
 
+        [EnumMember(Value = "KeycloakSAML")]
+        KeycloakSAML,
+
+        [EnumMember(Value = "LastPassSAML")]
+        LastPassSAML,
+
         [EnumMember(Value = "MagicLink")]
         MagicLink,
+
+        [EnumMember(Value = "MicrosoftOAuth")]
+        MicrosoftOAuth,
+
+        [EnumMember(Value = "MiniOrangeSAML")]
+        MiniOrangeSAML,
+
+        [EnumMember(Value = "NetIqSAML")]
+        NetIqSAML,
 
         [EnumMember(Value = "OktaSAML")]
         OktaSAML,
@@ -46,14 +77,29 @@
         [EnumMember(Value = "OneLoginSAML")]
         OneLoginSAML,
 
+        [EnumMember(Value = "OracleSAML")]
+        OracleSAML,
+
         [EnumMember(Value = "PingFederateSAML")]
         PingFederateSAML,
 
         [EnumMember(Value = "PingOneSAML")]
         PingOneSAML,
 
+        [EnumMember(Value = "RipplingSAML")]
+        RipplingSAML,
+
         [EnumMember(Value = "SalesforceSAML")]
         SalesforceSAML,
+
+        [EnumMember(Value = "ShibbolethGenericSAML")]
+        ShibbolethGenericSAML,
+
+        [EnumMember(Value = "ShibbolethSAML")]
+        ShibbolethSAML,
+
+        [EnumMember(Value = "SimpleSamlPhpSAML")]
+        SimpleSamlPhpSAML,
 
         [EnumMember(Value = "VMwareSAML")]
         VMwareSAML,
