2c3523dd77bb5f98859109293af27eb9a20d196a
Patrick_Liao <patrick@workos.com>
2022-09-01 12:27:41 -0500

    bump client version

--- src/WorkOS.net/Client/WorkOSClient.cs
+++ src/WorkOS.net/Client/WorkOSClient.cs
@@ -32,7 +32,7 @@
         /// <summary>
         /// Describes the .NET SDK version.
         /// </summary>
-        public static string SdkVersion => "1.10.0";
+        public static string SdkVersion => "1.11.0";
 
         /// <summary>
         /// Default timeout for HTTP requests.
