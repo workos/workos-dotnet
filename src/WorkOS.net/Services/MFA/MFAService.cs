a84460c85661aa00e254c188f3c63e36f154fd1e
Patrick_Liao <patrick@workos.com>
2022-10-18 16:05:07 -0500

    Remove return description from DeleteFactor

--- src/WorkOS.net/Services/MFA/MFAService.cs
+++ src/WorkOS.net/Services/MFA/MFAService.cs
@@ -150,7 +150,6 @@ namespace WorkOS
         /// <param name="cancellationToken">
         /// An optional token to cancel the request.
         /// </param>
-        /// <returns>A deleted Factor record.</returns>
         public async Task DeleteFactor(string id, CancellationToken cancellationToken = default)
         {
             var request = new WorkOSRequest
