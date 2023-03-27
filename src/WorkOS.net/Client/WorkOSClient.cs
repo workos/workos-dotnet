431f442488a614583293c264fce6a34e772711c5
Patrick_Liao <patrick@workos.com>
2023-03-27 11:40:04 -0400

    1.17.2

--- src/WorkOS.net/Client/WorkOSClient.cs
+++ src/WorkOS.net/Client/WorkOSClient.cs
@@ -32,7 +32,7 @@
         /// <summary>
         /// Describes the .NET SDK version.
         /// </summary>
-        public static string SdkVersion => "1.17.1";
+        public static string SdkVersion => "1.17.2";
 
         /// <summary>
         /// Default timeout for HTTP requests.
