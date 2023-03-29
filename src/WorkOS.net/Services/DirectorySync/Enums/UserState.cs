e7259d80114cff5db030cd6d3d6a563331b25e6d
Ash <44040482+AshGodfrey@users.noreply.github.com>
2023-03-29 12:16:35 -0600

    trailing whitespace

--- src/WorkOS.net/Services/DirectorySync/Enums/UserState.cs
+++ src/WorkOS.net/Services/DirectorySync/Enums/UserState.cs
@@ -12,10 +12,8 @@
     {
         [EnumMember(Value = "active")]
         Active,
-        
         [EnumMember(Value = "inactive")]
         Inactive,
-        
         [EnumMember(Value = "suspended")]
         Suspended,
     }
