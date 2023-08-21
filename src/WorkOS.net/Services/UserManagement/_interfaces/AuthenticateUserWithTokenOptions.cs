c282730ea18948bf6e6ff1a917b2da5fcbf32bd9
Patrick_Liao <patrick@workos.com>
2023-08-20 22:22:10 -0400

    Add AuthenticateUserWithToken

---
+++ src/WorkOS.net/Services/UserManagement/_interfaces/AuthenticateUserWithTokenOptions.cs
@@ -0,0 +1,52 @@
+namespace WorkOS
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// The parameters to authenticate a user with a token.
+    /// </summary>
+    public class AuthenticateUserWithTokenOptions : BaseOptions
+    {
+        /// <summary>
+        /// Specifies the grant type. Will always be `authorization_code`.
+        /// </summary>
+        [JsonProperty("grant_type")]
+        public const string GrantType = "authorization_code";
+
+        /// <summary>
+        /// This value can be obtained from the Configuration page in the WorkOS dashboard.
+        /// </summary>
+        [JsonProperty("client_id")]
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// This is the same value as the WorkOS Environment’s secret API key.
+        /// </summary>
+        [JsonProperty("client_secret")]
+        public string ClientSecret { get; set; }
+
+        /// <summary>
+        /// The authorization value which was passed back as a query parameter in the callback to the Redirect URI.
+        /// </summary>
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// The IP address of the request from the user who is attempting to authenticate.
+        /// </summary>
+        [JsonProperty("ip_address")]
+        public string IpAddress { get; set; }
+
+        /// <summary>
+        /// The user agent of the request from the user who is attempting to authenticate. This should be the value of the User-Agent header.
+        /// </summary>
+        [JsonProperty("user_agent")]
+        public string UserAgent { get; set; }
+
+        /// <summary>
+        /// The length of the session in minutes. Defaults to 1 day, 1440.
+        /// </summary>
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+    }
+}
