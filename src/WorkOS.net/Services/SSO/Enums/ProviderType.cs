97ba0d6c2df3d0e277bd51c6d552531a0f32b8fe
Oliver Zheng <oliver@workos.com>
2023-12-12 15:27:02 -0800

    Add new provider type

---
+++ src/WorkOS.net/Services/SSO/Enums/ProviderType.cs
@@ -0,0 +1,22 @@
+﻿namespace WorkOS
+{
+    using System.Runtime.Serialization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    /// <summary>
+    /// Constants that enumerate available OAuth provider types to initiate OAuth authentication.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ProviderType
+    {
+        [EnumMember(Value = "GitHubOAuth")]
+        GitHubOAuth,
+
+        [EnumMember(Value = "GoogleOAuth")]
+        GoogleOAuth,
+
+        [EnumMember(Value = "MicrosoftOAuth")]
+        MicrosoftOAuth,
+    }
+}
